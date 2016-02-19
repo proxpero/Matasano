0f37c899fef396be8fe06002f8e9b3587b871125
Todd Olsen <todd.olsen@me.com>
2016-02-19 14:20:49 -0500

    removed preliminary work on Challenge 7

--- Matasano/main.swift
+++ Matasano/main.swift
@@ -11,19 +11,3 @@ import Foundation
 testLibrary()
 testSets()
 
-private let content: [String] = {
-    let bundle = NSBundle.mainBundle()
-    let url = bundle.URLForResource("7", withExtension: "txt")!
-    let content = try! String(contentsOfURL: url)
-    return content.componentsSeparatedByString("\n")
-}()
-
-private let ciphertext  = content.flatMap { $0.base64ToBytes }
-private let keysize     = ciphertext.probableKeysize()
-
-private var decoded = [[UInt8]]()
-for block in ciphertext.blockify(keysize).transpose() {
-    decoded.append(block.decryptHexBytes())
-}
-
-private let result = decoded.transpose().flatMap { $0 }
